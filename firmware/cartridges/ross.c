787a6f591bcc7f344e7178d7508bea2945e3a365
nikvla <nivladan@yahoo.com>
2025-02-18 21:31:16 +0100

    Update ross.c

--- firmware/cartridges/ross.c
+++ firmware/cartridges/ross.c
@@ -79,7 +79,8 @@ FORCE_INLINE void ross_write_handler(u32 control, u32 addr, u32 data)
 static void ross_init()
 {
     
-    // C64_CRT_CONTROL(STATUS_LED_ON|CRT_PORT_16K);
+    C64_CRT_CONTROL(STATUS_LED_ON|CRT_PORT_16K);
+    crt_ptr = crt_banks[0];
 }
 
 C64_BUS_HANDLER(ross)
